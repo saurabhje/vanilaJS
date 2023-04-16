e487837e3ff70b6d375c68c5df65bdfc5c08a7b5
saurabhje <work.saurabh@outlook.com>
2023-04-16 15:32:25 +0530

    added extra credit

--- linkedList.js
+++ linkedList.js
@@ -64,13 +64,76 @@ class LinkedList{
         previousNode.next = null;      
     }
     contains(value){
-
+        let current = this.head;
+        if(!this.head){
+            return false;
+        }
+        while(current){
+            if(current.value == value){
+                return true;
+            }
+            current = current.next;
+        }
+        return false;
     }
     find(value){
-
+        let current = this.head;
+        let index = 0;
+        if(!this.head){
+            return;
+        }
+        while(current){
+            if(current.value == value){
+                return index;
+            }
+            current = current.next;
+            index++;
+        }
+        return "No such value in the list";
     }
     toString(){
-        
+        let current = this.head;
+        let result = "";
+        while(current){
+            result += `(${current.value}) -> `;
+            current = current.next;
+        }
+        result += "null";
+        return result;
+    }
+    insertAt(value, index){
+        let newNode = new Node(value);
+        if(index == 0){
+            newNode.next = this.head;
+            this.head = newNode;
+            return;
+        }
+        if(!this.head){
+            this.head = newNode;
+            return;
+        }
+        let previousNode ;
+        let current = this.head;
+        for(let kount = 0;kount<index;kount++){
+            previousNode = current;
+            current = current.next; 
+        }
+        previousNode.next = newNode;
+        newNode.next = current;
+    }
+    remove(index){
+        let current = this.head;
+        if(index ==0){
+            this.head = current.next
+            return;
+        }
+        let previousNode;
+        for(let i =0;i<index;i++){
+            previousNode = current;
+            current = current.next;
+        }
+        previousNode.next = current.next;
+        return;
     }
 }
 
@@ -81,6 +144,8 @@ class Node{
     }
 }
 
+
+// testing
 const list = new LinkedList();
 list.prepend(2);
 console.log(list.Head());
@@ -93,6 +158,11 @@ console.log(list.Head());
 list.append(3);
 console.log(list.tail());
 list.append(4);
+list.insertAt(9,0);
+console.log(list.contains(3));
 console.log(list.tail());
 list.pop();
 console.log(list.tail());
+console.log(list.find(5));
+list.remove(0);
+console.log(list.toString());
